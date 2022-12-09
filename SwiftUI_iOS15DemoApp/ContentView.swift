b06c0c3423892c79cd09c03d01cff8bc21512dbe
StarHuiDream <starhuidream@yeah.net>
2022-12-09 11:34:21 +0800

    Initial Commit

---
+++ SwiftUI_iOS15DemoApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_iOS15DemoApp
+//
+//  Created by nazimei on 2022/12/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_iOS15DemoApp/SwiftUI_iOS15DemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_iOS15DemoAppApp.swift
+//  SwiftUI_iOS15DemoApp
+//
+//  Created by nazimei on 2022/12/9.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_iOS15DemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
