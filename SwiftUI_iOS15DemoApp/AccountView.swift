fc1d46a17d8b665eb8bf3bd73f4b92419c5b1b5c
StarHuiDream <starhuidream@yeah.net>
2022-12-09 21:40:39 +0800

    Structure and Toggle

--- SwiftUI_iOS15DemoApp/AccountView.swift
+++ SwiftUI_iOS15DemoApp/AccountView.swift
@@ -8,43 +8,19 @@
 import SwiftUI
 
 struct AccountView: View {
+    @State var isDeleted = false
+    @State var ispinned = false
+    
+    var pinTitle: (title: String,imageName: String) {
+        ((ispinned ? "unpin" : "pin") , (ispinned ? "pin.slash" : "pin"))
+    }
+    
     var body: some View {
         NavigationView {
             List {
-                VStack (alignment: .center, spacing: 8) {
-                    Image(systemName: "person.crop.circle.fill.badge.checkmark")
-                        .symbolVariant(.circle.fill)
-                        .font(.system(size: 32))
-                        .symbolRenderingMode(.palette)
-                        .foregroundStyle(.blue, .blue.opacity(0.3))
-                        .padding()
-                        .background(Circle().fill(.ultraThinMaterial))
-                        .background(
-                            Image(systemName: "hexagon")
-                                .foregroundColor(.blue)
-                                .symbolVariant(.fill)
-                                .font(.system(size: 200))
-                                .offset(x: -50, y: -100)
-                        )
-                    Text("StarHui")
-                        .font(.title.weight(.semibold))
-                    HStack {
-                        Image(systemName: "location")
-                            .imageScale(.small)
-                        Text("China")
-                            .foregroundColor(.secondary)
-                    }
-                }
-                .frame(maxWidth: .infinity)
-                .padding()
-                
-                
-                Section {
-                    Label("Setting", systemImage: "gear")
-                        .foregroundStyle(.blue, .yellow)
-                    Label("Billing", systemImage: "creditcard")
-                    Label("Help", systemImage: "questionmark")
-                }.listRowSeparator(.hidden)
+                profile
+                settings
+                websites
             }
             .listStyle(.insetGrouped)
             .navigationBarTitle(
@@ -53,6 +29,84 @@ struct AccountView: View {
             .navigationBarTitleDisplayMode(.large)
         }
     }
+    
+    var profile : some View {
+        VStack (alignment: .center, spacing: 8) {
+            Image(systemName: "person.crop.circle.fill.badge.checkmark")
+                .symbolVariant(.circle.fill)
+                .font(.system(size: 32))
+                .symbolRenderingMode(.palette)
+                .foregroundStyle(.blue, .blue.opacity(0.3))
+                .padding()
+                .background(Circle().fill(.ultraThinMaterial))
+                .background(
+                    Image(systemName: "hexagon")
+                        .foregroundColor(.blue)
+                        .symbolVariant(.fill)
+                        .font(.system(size: 200))
+                        .offset(x: -50, y: -100)
+                )
+            Text("StarHui")
+                .font(.title.weight(.semibold))
+            HStack {
+                Image(systemName: "location")
+                    .imageScale(.small)
+                Text("China")
+                    .foregroundColor(.secondary)
+            }
+        }
+        .frame(maxWidth: .infinity)
+        .padding()
+    }
+    
+    var settings : some View {
+        Section {
+            NavigationLink { ContentView() } label: {
+                Label("Setting", systemImage: "gear")
+            }
+            NavigationLink { Label("Billing", systemImage: "creditcard") } label: {
+                Label("Billing", systemImage: "creditcard")
+            }
+            Label("Help", systemImage: "questionmark")
+        }.listRowSeparator(.hidden)
+            .accentColor(.primary)
+    }
+    
+    var websites : some View {
+        Section {
+            if !isDeleted {
+                Link(destination: URL(string: "https://www.starhui.cn")!) {
+                    HStack {
+                        Label("Website", systemImage: "house")
+                        Spacer()
+                        Image(systemName: "link")
+                            .foregroundColor(.secondary)
+                    }
+                }.swipeActions(edge: .leading, allowsFullSwipe: true) {
+                    Button(action: { isDeleted = true }) {
+                        Label("Delete", systemImage: "trash")
+                    }.tint(.red)
+                    pinButton
+                }
+            }
+            Link(destination: URL(string: "https://apple.com")!) {
+                HStack {
+                    Label("TEST", systemImage: "tv")
+                    Spacer()
+                    Image(systemName: "link")
+                        .foregroundColor(.secondary)
+                }
+            }
+        }
+        .accentColor(.primary)
+    }
+    
+    var pinButton: some View {
+        Button(action: { ispinned.toggle() }) {
+            Label(pinTitle.title, systemImage: pinTitle.imageName)
+        }.tint(.yellow)
+    }
+    
 }
 
 struct AccountView_Previews: PreviewProvider {
