885e9146953f43e05f00d48c7624d43e8ac11ac1
StarHui <starhuidream@yeah.net>
2022-12-12 11:23:26 +0800

    Rotation 3D effect (#9)

--- SwiftUI_iOS15DemoApp/HomeView.swift
+++ SwiftUI_iOS15DemoApp/HomeView.swift
@@ -12,15 +12,14 @@ struct HomeView: View {
     @State var isScrolled = false
     let coordinateSpaceName = "homeScroll"
     
-    
     var body: some View {
         ScrollView {
             GeometryReader { proxy in
-//                Text("\(proxy.frame(in: .named(coordinateSpaceName)).minY)")
-                Color.clear.preference(key: ScrollViewKey.self, value: proxy.frame(in: .named(coordinateSpaceName)).maxY)
+                //                Text("\(proxy.frame(in: .named(coordinateSpaceName)).minY)")
+                Color.clear.preference(key: ScrollViewKey.self, value: proxy.frame(in: .named(coordinateSpaceName)).minY)
             }
-//            .frame(height: 0)
-            FeatureItemView()
+            .frame(height: 0)
+            tabView
             Color.yellow.frame(height: 1000)
         }
         .coordinateSpace(name: coordinateSpaceName)
@@ -35,6 +34,36 @@ struct HomeView: View {
         .overlay(
             NavigationBarView(title: "Featured", isScrolled: $isScrolled)
         )
+    }
+    
+    var tabView: some View {
+        TabView{
+            ForEach(courses) { item in
+                GeometryReader { proxy in
+                    let minX = proxy.frame(in: .global).minX
+                    VStack{
+//                        Text("\(minX)")
+                        FeatureItemView(course: item)
+                            .padding(.vertical, 40)
+                            .rotation3DEffect(.degrees(minX / -10), axis: (x: 0, y: 1, z: 0))
+                            .shadow(color: Color("Shadow").opacity(0.8), radius: 10, x: 10, y: 0)
+                            .modifier(StrokeStyle(cornerRadius: 36))
+                            .blur(radius: abs(minX / 40))
+                            .overlay(
+                                Image(item.image)
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(height: 230)
+                                    .offset(x: 32, y: -80)
+                            )
+                    }
+                    .frame(maxWidth: .infinity)
+                }
+            }
+        }
+        .tabViewStyle(.page(indexDisplayMode: .never))
+        .frame(height: 430)
+        .background(Image("Blob 1").offset(x: 250, y: -100))
     }
 }
 
