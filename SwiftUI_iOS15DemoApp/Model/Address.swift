2f4263acc230c51b2ff062a19e5e60c898705768
StarHui <starhuidream@yeah.net>
2022-12-18 20:45:38 +0800

    Async & Await (#30)

---
+++ SwiftUI_iOS15DemoApp/Model/Address.swift
@@ -0,0 +1,13 @@
+//
+//  Address.swift
+//  SwiftUI_iOS15DemoApp
+//
+//  Created by nazimei on 2022/12/18.
+//
+
+import Foundation
+
+struct Address: Identifiable, Decodable {
+    var id: Int
+    var country: String
+}
